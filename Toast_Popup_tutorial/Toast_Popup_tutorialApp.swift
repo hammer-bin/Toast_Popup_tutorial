aa74ff9fb7e128f425f966536f6c69163324095e
suslmk <67575226+suslmk-lee@users.noreply.github.com>
2022-07-27 06:52:16 +0900

    Initial Commit

---
+++ Toast_Popup_tutorial/Toast_Popup_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Toast_Popup_tutorialApp.swift
+//  Toast_Popup_tutorial
+//
+//  Created by minkyuLee on 2022/07/27.
+//
+
+import SwiftUI
+
+@main
+struct Toast_Popup_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
